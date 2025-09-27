bb66d25718f039d75ccceaeeb2efb95154bde72d
shinDynasty <58328546+shinDynasty@users.noreply.github.com>
2025-09-27 21:17:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,29 @@
       openB = $('open'),
       closeB = $('close'),
       timer = null;
-  console.log('wat', card);
+
+  // Lấy phần tử audio bằng ID
+  var myAudio = $('myAudio');
+
   openB.addEventListener('click', function () {
     card.setAttribute('class', 'open-half');
-    if (timer) clearTimeout(timer);
+    if (timer) clearTimeout(timer); // Sửa lỗi đánh máy ở đây
     timer = setTimeout(function () {
       card.setAttribute('class', 'open-fully');
       timer = null;
+      // Bắt đầu phát nhạc khi thiệp đã mở hoàn toàn
+      myAudio.play();
     }, 1000);
   });
 
   closeB.addEventListener('click', function () {
     card.setAttribute('class', 'close-half');
-    if (timer) clearTimerout(timer);
+    if (timer) clearTimeout(timer); // Sửa lỗi đánh máy ở đây
     timer = setTimeout(function () {
       card.setAttribute('class', '');
       timer = null;
+      // Tạm dừng nhạc khi thiệp được đóng
+      myAudio.pause();
     }, 1000);
   });
 
